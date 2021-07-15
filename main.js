a545560f3cd79442aa77828a37297e3869c0971c
Karol <kalix321@gmail.com>
2021-07-15 19:13:02 +0200

    js added

---
+++ main.js
@@ -0,0 +1,14 @@
+const input = document.querySelector('input');
+const btn = document.querySelector('button');
+
+const cityName = document.querySelector('.city-name');
+const warning = document.querySelector('.warning');
+const photo = document.querySelector('.photo');
+
+const weather = document.querySelector('.weather');
+const temperature = document.querySelector('.temp');
+const humidity = document.querySelector('.humidity');
+
+const apiLink = 'api.openweathermap.org/data/2.5/weather?q=';
+
+const apiKey = '&APPID=88343978ddbb3a30e71c1dbda6fe2555';
